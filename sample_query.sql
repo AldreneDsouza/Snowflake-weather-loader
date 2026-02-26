0d75d70a6e1846601f18e28b618b02f6d067621d
Aldrene Joel Dsouza <aldrin.souza1999@gmail.com>
2026-02-26 12:14:43 +1030

    Add sample Snowflake query for weather project
    Added a sample query with a placeholder note for future weather data.

---
+++ sample_query.sql
@@ -0,0 +1,7 @@
+-- Sample Snowflake query for my weather project
+-- This is just a placeholder for now
+
+SELECT 
+  current_date() AS today,
+  'Loading weather data coming soon' AS note
+LIMIT 5;
